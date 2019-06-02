7ebb8a9f4ede2ed0ccfd576af62140a1f9866ecb
kai sun <sketernally@live.cn>
2019-06-02 13:07:37 +0800

    Update SMO inner loop

--- SVM/svmMLiA.py
+++ SVM/svmMLiA.py
@@ -104,3 +104,35 @@ def selectJ(i, oS, Ei):
 def updateEk(oS, k):
     Ek = clacEk(oS, k)
     oS.eCache[k] = [1, Ek]
+
+def innerL(i, oS):
+    Ei = clacEk(oS, i)
+    if ((oS.labelMat[i]*Ei < -oS.tol) and (oS.alphas[i] < oS.C)) or\
+        ((oS.labelMat[i]*Ei > oS.tol) and (oS.alphas[i] > 0)):
+        j,Ej = selectJ(i, oS, Ei)
+        alphaIold = oS.alphas[i].copy(); alphaJold = oS.alphas[j].copy()
+        if (oS.labelMat[i] != oS.labelMat[j]):
+            L = max(0, oS.alphas[j]-oS.alphas[i])
+            H = min(oS.C, oS.C+oS.alphas[j]-oS.alphas[i])
+        else:
+            L = max(0, oS.alphas[j]+oS.alphas[i]-oS.C)
+            H = min(oS.C, oS.alphas[j]+oS.alphas[i])
+        if L==H: print "L==H"; return 0
+        eta = 2.0 * oS.X[i,:]*oS.X[j,:] - oS.X[i,:]*oS.X[i,:].T - oS.X[j,:]*oS.X[j,:].T
+        if eta>=0: print "eta>=0"; return 0
+        oS.alphas[j] -= oS.labelMat[j]*(Ei-Ej)/eta
+        oS.alphas[j] = clipAlpha(oS.alphas[j], H, L)
+        updateEk(oS, j)
+        if (abs(oS.alphas[j] - alphaJold) < 0.00001):
+            print "j not moving enough"; return 0
+        oS.alphas[i] += oS.labelMat[j]*oS.labelMat[i]*(alphaJold - oS.alphas[j])
+        updateEk(oS, i)
+        b1 = oS.b - Ei - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*\
+             oS.X[i,:]*oS.X[i,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[i,:]*oS.X[i,:].T
+        b2 = oS.b - Ej - oS.labelMat[i]*(oS.alphas[i]-alphaIold)*\
+             oS.X[i,:]*oS.X[j,:].T - oS.labelMat[j]*(oS.alphas[j]-alphaJold)*oS.X[j,:]*oS.X[j,:].T
+        if (0 < oS.alphas[i]) and (oS.C > oS.alphas[i]): oS.b = b1
+        elif (0 < oS.alphas[j]) and (oS.C > oS.alphas[j]): oS.b = b2
+        else: oS.b = (b1 + b2)/2.0
+        return 1
+    else: return 0
